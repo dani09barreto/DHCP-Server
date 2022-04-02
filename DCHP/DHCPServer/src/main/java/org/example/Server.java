3a318a56aa7aea539f19f681c98a736a0ee286dd
dani09barreto <danielf.barreto@javeriana.edu.co>
2022-04-02 10:32:21 -0500

    se actualiza cliente

---
+++ DCHP/DHCPServer/src/main/java/org/example/Server.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import com.google.common.net.InetAddresses;
+import org.onlab.packet.DHCP;
+
+import java.io.IOException;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+
+public class Server {
+    public static void main( String[] args ) {
+        final int port = 67;
+        byte [] buffer = new byte[1024];
+
+        try {
+            System.out.println("servidor inciado");
+            DatagramSocket socketUDP = new DatagramSocket(port);
+
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+            socketUDP.receive(packet);
+            System.out.println("se recibe la info del cliete");
+            String mensaje = new String(packet.getData());
+            System.out.println(mensaje);
+
+            int clientPort = packet.getPort();
+            InetAddress direccion = packet.getAddress();
+            mensaje = "Servidor responde";
+            buffer = mensaje.getBytes(StandardCharsets.UTF_8);
+            DatagramPacket packetRequest = new DatagramPacket(buffer, buffer.length, direccion, clientPort);
+            System.out.println("se envia el mensaje");
+            socketUDP.send(packetRequest);
+
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
