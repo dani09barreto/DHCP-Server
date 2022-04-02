4af39fd445d651480f5009d191eb7c0ab62ea00a
dani09barreto <danielf.barreto@javeriana.edu.co>
2022-04-02 08:54:58 -0500

    Se añade maqueta java

---
+++ DCHP/DHCPServer/src/main/java/org/example/App.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.onlab.packet.DHCP;
+import org.onosproject.dhcp.DhcpService;
+import org.onosproject.dhcp.DhcpStore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        DHCP dhcp = new DHCP();
+    }
+}
