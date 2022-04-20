6a335fb745c7c7904511e5ce57c161e6c58fe167
dani09barreto <danielf.barreto@javeriana.edu.co>
2022-04-20 15:23:50 -0500

    Prueba de mensaje

--- DCHP/DHCPServer/src/main/java/org/example/Server.java
+++ DCHP/DHCPServer/src/main/java/org/example/Server.java
@@ -9,7 +9,7 @@ import java.nio.charset.StandardCharsets;
 
 public class Server {
     public static void main( String[] args ) {
-        final int port = 65000;
+        final int port = 68;
         byte [] buffer = new byte[1024];
 
         try {
@@ -19,16 +19,10 @@ public class Server {
             DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
             socketUDP.receive(packet);
             System.out.println("se recibe la info del cliete");
-            String mensaje = new String(packet.getData());
-            System.out.println(mensaje);
+            System.out.println("se convierte el mensaje");
+            DHCP mensaje = new DHCP();
+            mensaje.deserialize(packet.getData(), 4, packet.getData().length);
 
-            int clientPort = packet.getPort();
-            InetAddress direccion = packet.getAddress();
-            mensaje = "Servidor responde";
-            buffer = mensaje.getBytes(StandardCharsets.UTF_8);
-            DatagramPacket packetRequest = new DatagramPacket(buffer, buffer.length, direccion, clientPort);
-            System.out.println("se envia el mensaje");
-            socketUDP.send(packetRequest);
 
         } catch (SocketException e) {
             e.printStackTrace();
